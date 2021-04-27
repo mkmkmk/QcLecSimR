61f54d74d212fc7781d19fe513c3b01707c6c139
mkrej <mkrej>
2021-04-27 09:05:23 +0200

    ref. modpow() tests

--- modpow.R
+++ modpow.R
@@ -74,16 +74,15 @@ modpow = function(base, exponent, modulus)
     sapply(exponent, function(it) modpows(base, it, modulus))
 
 # modpow tests
-stopifnot(c(
-    modpow(5, 0, 13) == 1,
-    modpow(5, 1, 13) == 5,
-    modpow(777, 1, 13) == 777 %% 13,
-    modpow(5, 3, 13) == 5^3 %% 13,
-    modpow(2, 100, 7) == 2,
-    modpow(3, 100, 7) == 4,
-    modpow(10, 333, 5) == 0,
-    modpow(10, 333, 10) == 0,
-    modpow(10, 333, 2) == 0,
-    modpow(666, 0, 666) == 1
-))
+stopifnot(c(modpow(5, 0, 13) == 1))
+stopifnot(c(modpow(5, 1, 13) == 5))
+stopifnot(c(modpow(777, 1, 13) == 777 %% 13))
+stopifnot(c(modpow(5, 3, 13) == 5^3 %% 13))
+stopifnot(c(modpow(2, 100, 7) == 2))
+stopifnot(c(modpow(3, 100, 7) == 4))
+stopifnot(c(modpow(10, 333, 5) == 0))
+stopifnot(c(modpow(10, 333, 10) == 0))
+stopifnot(c(modpow(10, 333, 2) == 0))
+stopifnot(c(modpow(666, 0, 666) == 1))
+
 
