2c606ee6ffd77c9b65d07876fa05d3ea0a7e62e1
mkrej <mkrej>
2021-05-14 13:36:19 +0200

    update ad lec 9

--- qcLec09-superdense.R
+++ qcLec09-superdense.R
@@ -21,6 +21,15 @@ source("modpow.R")
 
 
 # ----- AD lecture 9 
+HI = kronecker(H,I)
+ZI = kronecker(Z,I)
+XI = kronecker(X,I)
+
+IH = kronecker(I,H)
+IZ = kronecker(I,Z)
+IX = kronecker(I,X)
+
+
 # superdense coding
 
 # eq. (9.1), ok, but I think you need to change the order in the last one
@@ -36,35 +45,50 @@ ZI %*% bell
 
 I %*% q1 
 
-XI %*% ZI %*% bell # mistake?
+XI %*% ZI %*% bell
 
+# eq. (9.2) --> ???
+bobT = matrix(c(1,1,0,0, 0,0,-1,1, 0,0,1,-1, 1,-1,0,0), 4) / sqrt(2)
 
-# eq. (9.2) --> mistake?
-kronecker(I, H)
-IH
-errBobT = matrix(c(1,1,0,0, 0,0,-1,1, 0,0,1,-1, 1,-1,0,0), 4) / sqrt(2)
-bobT = SWAP %*% HI %*% CNOT %*% SWAP 
+HI %*% RCX == bobT
+HI %*% CX == bobT
+IH %*% CX == bobT
+IH %*% RCX == bobT
 
-bobT == errBobT
+rBobT = HI %*% CNOT
+lBobT = SWAP %*% HI %*% CNOT %*% SWAP 
 
-sqrt(2) * errBobT
 sqrt(2) * bobT
+sqrt(2) * lBobT
+sqrt(2) * rBobT
 sqrt(2) * IH %*% SWAP %*% CNOT %*% SWAP  
 sqrt(2) * IH %*% RCX
 
-#decode - ok
+# rBobT - decode - ok
+stopifnot(q00 == round(lBobT %*% bell, 9))
+stopifnot(q01 == round(lBobT %*% ZI %*% bell, 9))
+stopifnot(q10 == round(lBobT %*% XI %*% bell, 9))
+stopifnot(q11 == round(lBobT %*% XI %*% ZI %*% bell, 9))
+
+# rBobT - decode - ok
+stopifnot(q00 == round(rBobT %*% bell, 9))
+stopifnot(q10 == round(rBobT %*% IZ %*% bell, 9))
+stopifnot(q01 == round(rBobT %*% IX %*% bell, 9))
+stopifnot(q11 == round(rBobT %*% IX %*% IZ %*% bell, 9))
+
+# bobT - not ok
 round(bobT %*% bell, 9)
 round(bobT %*% ZI %*% bell, 9)
 round(bobT %*% XI %*% bell, 9)
-round(bobT %*% XI %*% ZI %*% bell, 9)
+round(bobT %*% ZI %*% XI %*% bell, 9)
+round(bobT %*% bell, 9)
+round(bobT %*% IZ %*% bell, 9)
+round(bobT %*% IX %*% bell, 9)
+round(bobT %*% IZ %*% IX %*% bell, 9)
 
-if (FALSE)
-{
-    round(errBobT %*% XI %*% bell, 9)
-    round(errBobT %*% ZI %*% bell, 9)
-    round(errBobT %*% ZI %*% XI %*% bell, 9)
-}
-
-
+# unitarity test
+stopifnot(Mod(lBobT %*% t(Conj(lBobT)) - II) < epsilon)
+stopifnot(Mod(lBobT %*% t(Conj(lBobT)) - II) < epsilon)
+all(Mod(bobT %*% t(Conj(bobT)) - II) < epsilon) # (fail)
 
 
